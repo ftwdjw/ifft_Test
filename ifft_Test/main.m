8f043ca62926cbaa62bbfaed3325ce7cfd5a4777
ftwdjw <ftwdjw@gmail.com>
2014-06-29 09:14:04 -0500

    Initial Commit

---
+++ ifft_Test/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ifft_Test
+//
+//  Created by John Mac on 6/29/14.
+//  Copyright (c) 2014 John Wetters. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
